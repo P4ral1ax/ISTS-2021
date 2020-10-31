84eda2621d379ab1388f0b8e2e22c49702c06a1d
PapaChain <braydenjw00@gmail.com>
2020-10-30 22:03:11 -0400

    All firewall rules completed

--- iptables/Brayden_Boxes_(Tested)/hyde.sh
+++ iptables/Brayden_Boxes_(Tested)/hyde.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-# PF Rules Bruv
-# This is OpenBSD Box
+#########
+# Rules #
+#########
+# set skip on lo0
+# block all
+# pass in proto tcp to port { 22 80 }
+# pass in proto udp to port { 53 }
+# pass in inet proto icmp
+# pass out proto { tcp udp } to port { 22 53 80 }
+# pass out inet proto icmp
 
 start() {
-
-   
-    sleep 3
-    iptables -F
+    pfctl -f /etc/pf.conf
+    sleep 5
+    echo "" > /etc/pf.conf
+    pfctl -f /etc/pf.conf
 }
 start

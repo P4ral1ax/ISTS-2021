893a83e168454be3e8a7b95f76b282d537e2fa2a
PapaChain <braydenjw00@gmail.com>
2021-03-05 21:20:14 -0500

    update backup

--- quick_scripts/backup.sh
+++ quick_scripts/backup.sh
@@ -1,26 +1,36 @@
-!/bin/bash
+#!/bin/bash
 
 # Make Secret
-mkdir /home/$1/Pictures/.../
-mkdir /home/$1/Pictures/.../.../
-#ssh
-#Does it exist
-if [ -d "/etc/ssh" ] 
-then
-    # Tarring add dot?
-    tar -pcvf /home/$1/Pictures/.../.../.ssh.tar.gz /etc/ssh  > /dev/null  2>&1
-fi
-#web
-if [ -d "/var/www" ]
-then
-    # Tarring add dot?
-    tar -pcvf /home/$1/Pictures/.../.../.site.tar.gz /var/www > /dev/null 2>&1
-fi
-#custom
-if [ -d "/etc/$2" ]
-then
-    # Tarring add dot?
-    tar -pcvf /home/$1/Pictures/.../.../.$2.tar.gz /etc/$2  > /dev/null  2>&1
-fi
+mkdir -p /home/$1/Pictures/.../.../
 
-history -c
+all() {
+  dir_array=('/etc/ssh' '/var/www' '/etc/httpd' '/etc/nginx' '/etc/mysql' '/var/lib/mysql')
+  for i in "${dir_array[@]}"; do
+    if [ -d "$i" ] 
+    then
+      tar -pcvf /home/$1/Pictures/.../.../.$i.tar.gz $i  > /dev/null  2>&1
+    fi
+}
+
+one() {
+  echo -ne "Enter Absolute Directory : "
+  read dir
+  if [ -d "$2" ] 
+  then
+    tar -pcvf /home/$1/Pictures/.../.../.$2.tar.gz $2  > /dev/null  2>&1
+  fi
+
+}
+
+echo -ne "Enter Option (Default : None)\n1) All Directories\n2) Single Directory\n\n > "
+read opt
+
+# Run User Selected Mode
+if [[ $opt == 1 ]]; then 
+  basic
+  exit 0
+else
+  basic
+  verbose
+  exit 0
+fi
